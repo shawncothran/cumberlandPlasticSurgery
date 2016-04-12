5aa41852823c5ad05c73fc76a4817154230f41ac
Shawn Cothran <asliceofpizza@gmail.com>
2016-04-12 01:44:51 -0500

    flesh out modals and procedures

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,30 @@
 var $ = require('jQuery');
 
-$(document).ready(function(){
-  $(window).resize(function(){
-    $(document).scroll(function(){
+$(document).ready(function() {
+  $(window).resize(function() {
+    $(document).scroll(function() {
 
-      if( $(this).scrollTop() > $(window).height() - 80) {
+      if ($(this).scrollTop() > $(window).height() - 80) {
         $('.stickyNav').addClass('stickyNavScrolled');
+
       } else {
         $('.stickyNav').removeClass('stickyNavScrolled');
       }
 
-      $('.stickyNav__anchor').each(function(){
-        if ($(this.hash).position().top - 81 <= $(window).scrollTop()){
+      $('.stickyNav__anchor').each(function() {
+        if ($(this.hash).position().top - 81 <= $(window).scrollTop()) {
           $('.stickyNav__anchor.active').removeClass('active');
+
           $(this).addClass('active');
         }
       });
-
     });
   }).resize();
 });
 
 $(function() {
   $('a[href*="#"]:not([href="#"]):not([href*="#modal"])').click(function() {
-
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        || location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
